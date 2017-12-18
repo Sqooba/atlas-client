bc31293b21a1e187942ec5ec0ac4920ef9931234
Pietari Kettunen <pietari@sqooba.io>
2017-12-18 10:48:37 +0100

    external spec tagged

--- src/test/scala/io/sqooba/atlas/AtlasClientE2eSpec.scala
+++ src/test/scala/io/sqooba/atlas/AtlasClientE2eSpec.scala
@@ -1,12 +1,13 @@
 package io.sqooba.atlas
 
+import io.sqooba.ExternalSpec
 import org.scalatest.{AsyncFlatSpec, Matchers}
 
 class AtlasClientE2eSpec extends AsyncFlatSpec with Matchers {
 
   val aClient = new AtlasClient(new AtlasClientWrapper(), "http://dopintambari03.node.pmiint.ocean:21000")
 
-  "search entities" should "return SearchResult" in {
+  "search entities" should "return SearchResult" taggedAs (ExternalSpec) in {
     aClient.searchEntities("kafka_topic", "name=\"toptoptop\"").map(res => {
       println(res)
       1 shouldBe 1
